8e0e6796442ec11631a1549dbba78014740baf45
© ᴏꜰꜰɪᴄɪᴀʟ ᴋᴀᴠɪɴᴅᴜ-ᴍᴅ <113903400+Officialkavindu-MD@users.noreply.github.com>
2024-09-06 19:04:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const settings = require('./Settings');
-
-const client = new Client({
-  authStrategy: new LocalAuth({ clientId: settings.sessionId })
-});
-
-client.on('qr', qr => {
-    console.log('QR RECEIVED', qr);
-});
-
-client.on('ready', () => {
-    console.log('ALEXA-MD WHATSAPP BOT CONNECT TO YOUR WHATSAPP ✅');
-});
-
-client.initialize();
-
-
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const settings = require('./Settings');
 const fs = require('fs');
 const path = require('path');
 
